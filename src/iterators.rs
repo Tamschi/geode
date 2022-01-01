b59ad688fc2468f2b123671fdb59fe2cc62d5fac
Tamme Schichler <tamme@schichler.dev>
2022-01-01 16:25:05 +0100

    A lot of progress

--- src/iterators.rs
+++ src/iterators.rs
@@ -2,66 +2,86 @@
 
 use crate::{DynIteratee, DynIterateeMut, Iteratee, IterateeMut};
 use pretty_type_name::pretty_type_name;
-use std::{
+use core::{
 	fmt::{self, Debug, Formatter},
+	marker::PhantomData,
 	mem,
 };
 
 /// A dynamic dispatch iterator.
 #[derive(Clone)]
-pub struct Iter<I: DynIteratee> {
+pub struct Iter<'a, I: DynIteratee>
+where
+	I::Item: 'a,
+{
 	iteratee: I,
+	_phantom: PhantomData<&'a ()>,
 }
 
-impl<I: DynIteratee> Iter<I> {
+impl<I: DynIteratee> Iter<'_, I> {
 	/// Creates a new instance of [`Iter`] targeting the given [`Iteratee`].
 	#[must_use]
 	pub fn new(iteratee: I) -> Self {
-		Self { iteratee }
+		Self {
+			iteratee,
+			_phantom: PhantomData,
+		}
 	}
 }
 
 /// A mutating dynamic dispatch iterator.
 ///
 /// Note that this type is downgradeable into a [`Clone`] [`Iter`] via [`From`]/[`Into`] conversion.
-pub struct IterMut<I: DynIterateeMut> {
+pub struct IterMut<'a, I: DynIterateeMut>
+where
+	I::Item: 'a,
+{
 	iteratee: I,
+	_phantom: PhantomData<&'a ()>,
 }
 
-impl<I: DynIterateeMut> IterMut<I> {
+impl<I: DynIterateeMut> IterMut<'_, I> {
 	/// Creates a new instance of [`IterMut`] targeting the given [`IterateeMut`].
 	#[must_use]
 	pub fn new(iteratee: I) -> Self {
-		Self { iteratee }
+		Self {
+			iteratee,
+			_phantom: PhantomData,
+		}
 	}
 
 	/// Creates a new [`IterMut`] advancing independently of this one.
 	#[must_use]
-	pub fn fork(&mut self) -> IterMut<I> {
-		IterMut::new(self.iteratee)
+	pub fn fork(&mut self) -> IterMut<'_, I> {
+		IterMut::new(unsafe { (&mut self.iteratee as *mut I).read() })
 	}
 
 	/// Creates an [`Iter`] advancing independently of this one.
 	#[must_use]
 	pub fn fork_shared(&self) -> Iter<I::DynIteratee> {
-		Iter::new(self.iteratee.as_mut().as_iteratee())
+		Iter::new(unsafe {
+			(&self.iteratee.as_ref().as_iteratee() as *const &dyn Iteratee<I::Item>)
+				.cast::<I::DynIteratee>()
+				.read()
+		})
 	}
 }
 
-impl<I: DynIteratee> Iterator for Iter<I> {
-	type Item = I::Item;
+impl<'a, I: DynIteratee> Iterator for Iter<'a, I> {
+	type Item = &'a I::Item;
 
 	fn next(&mut self) -> Option<Self::Item> {
 		let (head, rest) = self.iteratee.as_ref().head_rest();
 		unsafe {
-			self.iteratee = mem::transmute(rest);
-			mem::transmute(head)
+			let head = mem::transmute(head);
+			self.iteratee = (&rest as *const &dyn Iteratee<I::Item>).cast::<I>().read();
+			head
 		}
 	}
 }
 
-impl<I: DynIterateeMut> Iterator for IterMut<I> {
-	type Item = I::Item;
+impl<'a, I: DynIterateeMut> Iterator for IterMut<'a, I> {
+	type Item = &'a mut I::Item;
 
 	fn next(&mut self) -> Option<Self::Item> {
 		let iteratee = &mut self.iteratee as *mut I;
@@ -74,28 +94,34 @@ impl<I: DynIterateeMut> Iterator for IterMut<I> {
 			// but we can replace it with `rest` whose target is guaranteed to be disjoint (as far as pointer restrictions go).
 			//
 			// TODO: Safety notes on the transmutes below.
-			let (head, rest) = iteratee.read().as_mut().head_rest_mut();
-			iteratee.write(mem::transmute(rest));
-			mem::transmute(head)
+			let mut iteratee_ = iteratee.read();
+			let (head, mut rest) = iteratee_.as_mut().head_rest_mut();
+			let head = mem::transmute(head);
+			iteratee.write(
+				(&mut rest as *mut &mut dyn IterateeMut<I::Item>)
+					.cast::<I>()
+					.read(),
+			);
+			head
 		}
 	}
 }
 
 /// Downgrades an [`IterMut`] into a ([`Clone`]) [`Iter`].
-impl<I: DynIterateeMut> From<IterMut<I>> for Iter<I::DynIteratee> {
+impl<'a, I: DynIterateeMut> From<IterMut<'a, I>> for Iter<'a, I::DynIteratee> {
 	fn from(iter_mut: IterMut<I>) -> Self {
 		Self::new(iter_mut.iteratee.into_dyn_iteratee())
 	}
 }
 
-impl<I: DynIteratee> Debug for Iter<I> {
+impl<I: DynIteratee> Debug for Iter<'_, I> {
 	fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
 		f.debug_struct(&pretty_type_name::<Self>())
 			.finish_non_exhaustive()
 	}
 }
 
-impl<I: DynIterateeMut> Debug for IterMut<I> {
+impl<I: DynIterateeMut> Debug for IterMut<'_, I> {
 	fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
 		f.debug_struct(&pretty_type_name::<Self>())
 			.finish_non_exhaustive()
--- src/lib.rs
+++ src/lib.rs
@@ -5,13 +5,13 @@
 #![doc(html_root_url = "https://docs.rs/geode/0.0.1")]
 #![warn(clippy::pedantic, missing_docs)]
 #![allow(clippy::semicolon_if_nothing_returned)]
+#![no_std]
 
 #[cfg(doctest)]
 #[doc = include_str!("../README.md")]
 mod readme {}
 
 pub mod iterators;
-pub mod thread_safety;
 
 //TODO: Macro to generate a custom list type with optional Cons and RCons implementations.
 //TODO: Macro to privately implement iteration.
@@ -46,53 +46,417 @@ pub unsafe trait IterateeMut<T: ?Sized>: Iteratee<T> {
 }
 
 mod private {
-	use std::ops::{Deref, DerefMut};
+	use core::ops::{Deref, DerefMut};
 
 	use crate::{DynIteratee, Iteratee, IterateeMut};
 
 	pub trait Sealed {}
-	impl<T: ?Sized> Sealed for &dyn Iteratee<T> {}
-	impl<T: ?Sized> Sealed for &mut dyn IterateeMut<T> {}
 
-	#[doc(hidden)]
-	pub trait DynIterateeImpl {
-		type Item: Deref;
-
-		fn as_ref(&self) -> &dyn Iteratee<<Self::Item as Deref>::Target>;
-	}
-	impl<'a, T: ?Sized> DynIterateeImpl for &'a dyn Iteratee<T> {
-		type Item = &'a T;
-
-		fn as_ref(&self) -> &dyn Iteratee<<Self::Item as Deref>::Target> {
-			*self
-		}
+	macro_rules! impl_sealed {
+		($(
+			$ty:ty
+		),*$(,)?) => {$(
+			impl<T: ?Sized> Sealed for $ty {}
+		)*};
 	}
 
+	impl_sealed!(
+		&dyn Iteratee<T>,
+		&(dyn Iteratee<T> + Send),
+		&(dyn Iteratee<T> + Sync),
+		&(dyn Iteratee<T> + Send + Sync),
+		&mut dyn IterateeMut<T>,
+		&mut (dyn IterateeMut<T> + Send),
+		&mut (dyn IterateeMut<T> + Sync),
+		&mut (dyn IterateeMut<T> + Send + Sync),
+	);
+
 	#[doc(hidden)]
-	pub trait DynIterateeMutImpl {
-		type Item: DerefMut;
+	pub trait DynIterateeImpl: Deref {
+		type Item: ?Sized;
+
+		fn as_ref(&self) -> &dyn Iteratee<Self::Item>;
+	}
+
+	macro_rules! impl_dyn_iteratee_impl {
+		($(
+			$ty:ty
+		),*$(,)?) => {$(
+			impl<'a, T: ?Sized> DynIterateeImpl for &'a $ty {
+				type Item = T;
+
+				fn as_ref(&self) -> &dyn Iteratee<Self::Item> {
+					*self
+				}
+			}
+		)*};
+	}
+
+	impl_dyn_iteratee_impl!(
+		dyn Iteratee<T>,
+		dyn Iteratee<T> + Send,
+		dyn Iteratee<T> + Sync,
+		dyn Iteratee<T> + Send + Sync,
+	);
+
+	#[doc(hidden)]
+	pub trait DynIterateeMutImpl: DerefMut {
+		type Item: ?Sized;
 		type DynIteratee: DynIteratee;
 
 		fn into_dyn_iteratee(self) -> Self::DynIteratee;
 
-		fn as_mut(&mut self) -> &mut dyn IterateeMut<<Self::Item as Deref>::Target>;
+		fn as_ref(&self) -> &dyn IterateeMut<Self::Item>;
+		fn as_mut(&mut self) -> &mut dyn IterateeMut<Self::Item>;
 	}
-	impl<'a, T: ?Sized> DynIterateeMutImpl for &'a mut dyn IterateeMut<T> {
-		type Item = &'a mut T;
-		type DynIteratee = &'a dyn Iteratee<T>;
 
-		fn into_dyn_iteratee(self) -> Self::DynIteratee {
-			self.as_iteratee()
-		}
+	macro_rules! impl_dyn_iteratee_mut_impl {
+		($(
+			$ty:ty
+		),*$(,)?) => {$(
+			impl<'a, T: ?Sized> DynIterateeMutImpl for &'a mut $ty {
+				type Item = T;
+				type DynIteratee = &'a dyn Iteratee<T>;
 
-		fn as_mut(&mut self) -> &mut dyn IterateeMut<<Self::Item as Deref>::Target> {
-			*self
-		}
+				fn into_dyn_iteratee(self) -> Self::DynIteratee {
+					self.as_iteratee()
+				}
+
+				fn as_ref(&self) -> &dyn IterateeMut<Self::Item> {
+					*self
+				}
+
+				fn as_mut(&mut self) -> &mut dyn IterateeMut<Self::Item> {
+					*self
+				}
+			}
+		)*};
 	}
+
+	impl_dyn_iteratee_mut_impl!(
+		dyn IterateeMut<T>,
+		dyn IterateeMut<T> + Send,
+		dyn IterateeMut<T> + Sync,
+		dyn IterateeMut<T> + Send + Sync,
+	);
 }
+use core::{convert::Infallible, mem::ManuallyDrop};
+
+use iterators::{Iter, IterMut};
 use private::{DynIterateeImpl, DynIterateeMutImpl, Sealed};
 
 pub trait DynIteratee: Sealed + DynIterateeImpl {}
 impl<T: ?Sized> DynIteratee for &dyn Iteratee<T> {}
+impl<T: ?Sized> DynIteratee for &(dyn Iteratee<T> + Send) {}
+impl<T: ?Sized> DynIteratee for &(dyn Iteratee<T> + Sync) {}
+impl<T: ?Sized> DynIteratee for &(dyn Iteratee<T> + Send + Sync) {}
 pub trait DynIterateeMut: Sealed + DynIterateeMutImpl {}
 impl<T: ?Sized> DynIterateeMut for &mut dyn IterateeMut<T> {}
+impl<T: ?Sized> DynIterateeMut for &mut (dyn IterateeMut<T> + Send) {}
+impl<T: ?Sized> DynIterateeMut for &mut (dyn IterateeMut<T> + Sync) {}
+impl<T: ?Sized> DynIterateeMut for &mut (dyn IterateeMut<T> + Send + Sync) {}
+
+pub trait DynIter {
+	type DynIteratee: DynIteratee;
+	fn dyn_iter(&self) -> Iter<'_, Self::DynIteratee>;
+}
+pub trait DynIterMut {
+	type DynIterateeMut: DynIterateeMut;
+	fn dyn_iter_mut(&self) -> IterMut<'_, Self::DynIterateeMut>;
+}
+
+pub trait StaticIter<T: ?Sized> {
+	fn try_for_each<E>(self, on_each: impl FnMut(T) -> Result<(), E>) -> Result<(), E>
+	where
+		T: Sized;
+	fn try_for_each_ref<E>(&self, on_each: impl FnMut(&T) -> Result<(), E>) -> Result<(), E>;
+	fn try_for_each_mut<E>(
+		&mut self,
+		on_each: impl FnMut(&mut T) -> Result<(), E>,
+	) -> Result<(), E>;
+
+	fn for_each(self, mut on_each: impl FnMut(T))
+	where
+		Self: Sized,
+		T: Sized,
+	{
+		self.try_for_each(move |item| Ok::<_, Infallible>(on_each(item)))
+			.unwrap()
+	}
+	fn for_each_ref(&self, mut on_each: impl FnMut(&T)) {
+		self.try_for_each_ref(move |item| Ok::<_, Infallible>(on_each(item)))
+			.unwrap()
+	}
+	fn for_each_mut(&mut self, mut on_each: impl FnMut(&mut T)) {
+		self.try_for_each_mut(move |item| Ok::<_, Infallible>(on_each(item)))
+			.unwrap()
+	}
+
+	fn try_fold<A, E, F: FnMut(A, T) -> Result<A, E>>(
+		self,
+		initial: A,
+		mut on_fold: F,
+	) -> Result<A, E>
+	where
+		Self: Sized,
+		T: Sized,
+	{
+		let mut aggregate = ManuallyDrop::new(initial);
+		let aggregate_ = &mut *aggregate as *mut A;
+		self.try_for_each(move |item| unsafe {
+			Ok(aggregate_.write(on_fold(aggregate_.read(), item)?))
+		})?;
+		Ok(ManuallyDrop::into_inner(aggregate))
+	}
+
+	fn try_fold_ref<A, E, F: FnMut(A, &T) -> Result<A, E>>(
+		&self,
+		initial: A,
+		mut on_fold: F,
+	) -> Result<A, E>
+	where
+		Self: Sized,
+		T: Sized,
+	{
+		let mut aggregate = ManuallyDrop::new(initial);
+		let aggregate_ = &mut *aggregate as *mut A;
+		self.try_for_each_ref(move |item| unsafe {
+			Ok(aggregate_.write(on_fold(aggregate_.read(), item)?))
+		})?;
+		Ok(ManuallyDrop::into_inner(aggregate))
+	}
+
+	fn try_fold_mut<A, E, F: FnMut(A, &mut T) -> Result<A, E>>(
+		&mut self,
+		initial: A,
+		mut on_fold: F,
+	) -> Result<A, E>
+	where
+		Self: Sized,
+		T: Sized,
+	{
+		let mut aggregate = ManuallyDrop::new(initial);
+		let aggregate_ = &mut *aggregate as *mut A;
+		self.try_for_each_mut(move |item| unsafe {
+			Ok(aggregate_.write(on_fold(aggregate_.read(), item)?))
+		})?;
+		Ok(ManuallyDrop::into_inner(aggregate))
+	}
+
+	fn fold<A, E, F: FnMut(A, T) -> A>(self, initial: A, mut on_fold: F) -> A
+	where
+		Self: Sized,
+		T: Sized,
+	{
+		self.try_fold(initial, move |aggregate, item| {
+			Ok::<_, Infallible>(on_fold(aggregate, item))
+		})
+		.unwrap()
+	}
+
+	fn fold_ref<A, E, F: FnMut(A, &T) -> A>(&self, initial: A, mut on_fold: F) -> A
+	where
+		Self: Sized,
+		T: Sized,
+	{
+		self.try_fold_ref(initial, move |aggregate, item| {
+			Ok::<_, Infallible>(on_fold(aggregate, item))
+		})
+		.unwrap()
+	}
+
+	fn fold_mut<A, E, F: FnMut(A, &mut T) -> A>(&mut self, initial: A, mut on_fold: F) -> A
+	where
+		Self: Sized,
+		T: Sized,
+	{
+		self.try_fold_mut(initial, move |aggregate, item| {
+			Ok::<_, Infallible>(on_fold(aggregate, item))
+		})
+		.unwrap()
+	}
+}
+
+#[doc(hidden)]
+pub mod __ {
+	#[doc(hidden)]
+	#[macro_export]
+	macro_rules! custom_list_types {
+		(
+			$(#[$listMeta:meta])*
+			$listVis:vis struct $List:ident
+				$([$($generics:tt)*][$($generics0:tt)*])?
+				$(where [$($constraints:tt)*][$($constraints2:tt)*])?
+			{
+				$listFieldVis:vis head: $itemTy:ty,
+				..
+			},
+
+			$(#[$endMeta:meta])*
+			$endVis:vis struct $End:ident,
+
+			$(#[$consMeta:meta])*
+			$consVis:vis trait $Cons:ident$(,
+				$wrapperVis:vis struct $Wrapper:ident
+			)?$(,)?
+		) => {
+			$(#[$listMeta])*
+			$listVis struct $List<$($($generics)*,)? R>
+				$(where $($constraints)*,)?
+			{
+				/// The first item of the list.
+				$listFieldVis head: $itemTy,
+
+				/// The rest of the list.
+				$listFieldVis rest: R,
+			}
+
+			$(#[$endMeta])*
+			$endVis struct End;
+
+			$(#[$consMeta])*
+			$consVis trait Cons$(<$($generics)*>)?
+				$(where
+					$($constraints)*,
+					$($constraints2)*,
+				)?
+			{
+				/// The result of a `cons` operation, with `T` prepended as head.
+				type Cons;
+				/// The results of an `r_cons` operation, with `T` appended as tail.
+				type RCons;
+
+				/// Transforms `self` by prepending `head`.
+				fn cons(self, head: $itemTy) -> Self::Cons;
+
+				/// Transforms `self` by appending `tail`.
+				fn r_cons(self, tail: $itemTy) -> Self::RCons;
+			}
+		};
+	}
+	pub use custom_list_types;
+}
+
+/// Creates and implements a custom cons list.
+#[macro_export]
+macro_rules! custom_list {
+	(
+		$(#[$listMeta:meta])*
+		$listVis:vis struct $List:ident
+			$([$($generics:tt)*][$($generics0:tt)*])?
+			$(where [$($constraints:tt)*][$($constraints2:tt)*])?
+		{
+			$listFieldVis:vis head: $itemTy:ty,
+			..
+		},
+
+		$(#[$endMeta:meta])*
+		$endVis:vis struct $End:ident,
+
+		$(#[$consMeta:meta])*
+		$consVis:vis trait $Cons:ident
+		$(,)?
+	) => {
+		$crate::__::custom_list_types!(
+			$(#[$listMeta])* $listVis struct $List$([$($generics)*][$($generics0)*])? {
+				$listFieldVis head: $itemTy,
+				..
+			},
+			$(#[$endMeta])* $endVis struct $End,
+			$(#[$consMeta])* $consVis trait $Cons,
+		);
+
+		impl$(<$($generics)*>)? $Cons$(<$($generics)*>)? for $End {
+			type Cons = $List<$($($generics)*,)? Self>;
+			type RCons = $List<$($($generics)*,)? Self>;
+			fn cons(self, head: $itemTy) -> <Self as $Cons$(<$($generics)*>)?>::Cons {
+				$List {
+					head,
+					rest: self,
+				}
+			}
+			fn r_cons(self, tail: $itemTy) -> <Self as $Cons$(<$($generics)*>)?>::RCons {
+				$List {
+					head: tail,
+					rest: self,
+				}
+			}
+  		}
+
+		impl<
+			$(
+				$($generics)*,
+				$($generics0)*,
+			)?
+			R: $Cons$(<$($generics)*>)?,
+		> $Cons$(<$($generics)*>)? for $List<$($($generics0)*,)? R> {
+			type Cons = $List<$($($generics)*,)? Self>;
+			type RCons = $List<$($($generics0)*,)? <R as $Cons$(<$($generics)*>)?>::RCons>;
+			fn cons(self, head: $itemTy) -> <Self as $Cons$(<$($generics)*>)?>::Cons {
+				$List {
+					head,
+					rest: self,
+				}
+			}
+			fn r_cons(self, tail: $itemTy) -> <Self as $Cons$(<$($generics)*>)?>::RCons {
+				$List {
+					head: self.head,
+					rest: <R as $Cons$(<$($generics)*>)?>::r_cons(self.rest, tail),
+				}
+			}
+		}
+
+		impl<X: ?Sized> $crate::StaticIter<X> for $End {
+			fn try_for_each<E>(
+				self,
+				_: impl ::core::ops::FnMut(X) -> ::core::result::Result<(), E>,
+			) -> ::core::result::Result<(), E> where X: Sized {
+				Ok(())
+			}
+			fn try_for_each_ref<E>(
+				&self,
+				_: impl ::core::ops::FnMut(&X) -> ::core::result::Result<(), E>,
+			) -> ::core::result::Result<(), E> {
+				Ok(())
+			}
+			fn try_for_each_mut<E>(
+				&mut self,
+				_: impl ::core::ops::FnMut(&mut X) -> ::core::result::Result<(), E>,
+			) -> ::core::result::Result<(), E> {
+				Ok(())
+			}
+  		}
+	};
+
+	// (
+	// 	$(#[$listMeta:meta])*
+	// 	$listVis:vis $List:ident $({$listFieldVis:vis})?,
+
+	// 	$(#[$endMeta:meta])*
+	// 	$endVis:vis $End:ident,
+
+	// 	$(#[$consMeta:meta])*
+	// 	$consVis:vis $Cons:ident,
+
+	// 	$wrapperVis:vis $Wrapper:ident
+	// 	$(,)?
+	// ) => {
+	// 	compile_error!("TODO: Cons list with wrapper.")
+	// };
+}
+
+custom_list!(
+	/// A basic cons list with public implementation details.
+	///
+	/// Use [`Cons`] methods on [`End`] to start constructing a [`List`].
+	pub struct List[T][T0] {
+		pub head: T,
+		..
+	},
+
+	/// An empty rest for [`List`].
+	pub struct End,
+
+	/// Builder functionality for [`List`] and [`End`].
+	pub trait Cons,
+);
